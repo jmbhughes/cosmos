e26e0ded983bfebef19e8e75682aad6dfeba2323
Marcus Hughes <hughes.jmb@gmail.com>
2023-06-24 16:00:43 -0600

    basic uw ca working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod uw;
+
 fn main() {
-    println!("Hello, world!");
+    let mut my_ca = uw::UlamWarburtonCA::new(100, 100);
+    my_ca.set(49, 49);
+    my_ca.iterate(8);
+    my_ca.show();
 }
---
+++ src/uw.rs
@@ -0,0 +1,58 @@
+use ndarray::{Array2};
+use ndarray_conv::*;
+
+
+pub struct UlamWarburtonCA {
+    num_rows: usize,
+    num_cols: usize,
+    grid: Array2<u8>
+}
+
+impl UlamWarburtonCA {
+    pub fn new(rows: usize, cols: usize) -> Self {
+        Self {num_rows: rows, num_cols: cols, grid: Array2::zeros((rows, cols))}
+    }
+
+    pub fn set(&mut self, row: usize, col: usize) {
+        self.grid[[row, col]] = 1;
+    }
+
+    pub fn unset(&mut self, row: usize, col: usize) {
+        self.grid[[row, col]] = 0;
+    }
+
+    pub fn step(&mut self) {
+        let kernel =Array2::from_shape_vec((3, 3),  
+            vec![0, 1, 0, 1, 0, 1, 0, 1, 0]).unwrap();
+        let grow_mask = self.grid.conv_2d(&kernel, 
+            PaddingSize::Same, 
+            PaddingMode::Zeros).unwrap().map(|&v| v == 1u8);
+        for i in 0..self.num_rows {
+            for j in 0..self.num_cols {
+                if grow_mask[[i, j]] {
+                    self.grid[[i, j]] = 1;
+                }
+            }
+        }
+    }
+
+    pub fn iterate(&mut self, iterations: usize) {
+        for _ in 0..iterations {
+            self.step();
+        }
+    }
+
+    pub fn show(self) {
+        for i in 0..self.num_rows {
+            for j in 0..self.num_cols {
+                print!("{}", {match self.grid[[i,j]] {
+                    0 => " ",
+                    1 => "█",
+                    _ => " "
+                }});
+            }
+            println!();
+        }
+    }
+
+}
